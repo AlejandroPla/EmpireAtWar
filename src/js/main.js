c149629e4cdd1f9fd79868c1d84b3909cf0b5f10
AlejandroPla <aplazer@ucm.es>
2019-01-05 08:02:05 +0100

    añadido el menu

--- src/js/main.js
+++ src/js/main.js
@@ -13,64 +13,126 @@ var BootScene = {
   }
 };
 
-
 var PreloaderScene = {
   preload: function () {
     this.loadingBar = this.game.add.sprite(0, 240, 'preloader_bar');
     this.loadingBar.anchor.setTo(0, 0.5);
     this.load.setPreloadSprite(this.loadingBar);
 
-    // TODO: load here the assets for the game
-    this.game.load.image('logo', 'images/phaser.png');
-    this.game.load.tilemap('level_01', 'resources/maps/level_01.Json', null, Phaser.Tilemap.TILED_JSON);
-    this.game.load.image('Tile-set', 'resources/sprites/Tile-set.png');
-    this.game.load.image('Colored_Tiles', 'resources/sprites/Colored_Tiles.png' )
-    this.game.load.image('nextTurnIcon', 'resources/sprites/Hud/nextTurnIcon.png');
-    this.game.load.image('unitIcon', 'resources/sprites/Hud/unitIcon.png');
-    this.game.load.image('structureIcon', 'resources/sprites/Hud/structureIcon.png');
-    this.game.load.image('inventoryBackground', 'resources/sprites/Hud/Inventory_Background.png');
-    this.game.load.image('statsBackground', 'resources/sprites/Hud/Stats_Background.png');
-    this.game.load.image('empty', 'resources/sprites/Hud/Empty.png');
-    this.game.load.spritesheet('coinIcon', 'resources/sprites/Hud/coin.png', 120, 96, 6);
+    this.game.load.image('logo', 'images/phaser.png');  
 
-    //UNITS
-    this.game.load.image('Red_Peasant', 'resources/sprites/Hud/Red_Peasant.png');
-    this.game.load.image('Red_Lancer', 'resources/sprites/Hud/Red_Lancer.png');
-    this.game.load.image('Red_Swordman', 'resources/sprites/Hud/Red_Swordman.png');
-    this.game.load.image('Red_Horseman', 'resources/sprites/Hud/Red_Horseman.png');
-    this.game.load.image('Yellow_Peasant', 'resources/sprites/Hud/Yellow_Peasant.png');
-    this.game.load.image('Yellow_Lancer', 'resources/sprites/Hud/Yellow_Lancer.png');
-    this.game.load.image('Yellow_Swordman', 'resources/sprites/Hud/Yellow_Swordman.png');
-    this.game.load.image('Yellow_Horseman', 'resources/sprites/Hud/Yellow_Horseman.png');
-
-    //STRUCTURES
-    this.game.load.image('Red_Farm', 'resources/sprites/Hud/Red_Farm.png')
-    this.game.load.image('Red_Tower', 'resources/sprites/Hud/Red_Tower.png');
-    this.game.load.image('Red_Fortress', 'resources/sprites/Hud/Red_Fortress.png');
-    this.game.load.image('Red_Base', 'resources/sprites/Hud/Red_Base.png');
-    this.game.load.image('Yellow_Farm', 'resources/sprites/Hud/Yellow_Farm.png');
-    this.game.load.image('Yellow_Tower', 'resources/sprites/Hud/Yellow_Tower.png');
-    this.game.load.image('Yellow_Fortress', 'resources/sprites/Hud/Yellow_Fortress.png');
-    this.game.load.image('Yellow_Base', 'resources/sprites/Hud/Yellow_Base.png');
-
-    //INDICATORS
-    this.game.load.image('movement', 'resources/sprites/Movement_Indicator.png');
-    this.game.load.image('nope', 'resources/sprites/Nope_Indicator.png');
-    this.game.load.image('combat', 'resources/sprites/Combat_Indicator.png');
-
+    this.game.load.image('playButton', 'resources/menu/playButton.png');
+    this.game.load.image('rulesButton', 'resources/menu/rulesButton.png');
+    this.game.load.image('backgroundImage', 'resources/menu/backgroundImage.png');
+    this.game.load.image('aboutUs', 'resources/menu/aboutUs.png');
+    this.game.load.image('aboutUsButton', 'resources/menu/aboutUsButton.png');
+    this.game.load.image('backButton', 'resources/menu/backButton.png');
+    this.game.load.image('rules', 'resources/menu/rules.png');
+    this.game.load.image('endGame', 'resources/menu/endGame.png');
   },
 
   create: function () {
-    this.game.state.start('play');
+    this.game.state.start('MainMenu');
   }
 };
 
+var MenuScene={
+  preload: function(){
+     // TODO: load here the assets for the game
+     this.game.load.image('logo', 'images/phaser.png');
+     this.game.load.tilemap('level_01', 'resources/maps/level_01.Json', null, Phaser.Tilemap.TILED_JSON);
+     this.game.load.image('Tile-set', 'resources/sprites/Tile-set.png');
+     this.game.load.image('Colored_Tiles', 'resources/sprites/Colored_Tiles.png' )
+     this.game.load.image('nextTurnIcon', 'resources/sprites/Hud/nextTurnIcon.png');
+     this.game.load.image('unitIcon', 'resources/sprites/Hud/unitIcon.png');
+     this.game.load.image('structureIcon', 'resources/sprites/Hud/structureIcon.png');
+     this.game.load.image('inventoryBackground', 'resources/sprites/Hud/Inventory_Background.png');
+     this.game.load.image('statsBackground', 'resources/sprites/Hud/Stats_Background.png');
+     this.game.load.image('empty', 'resources/sprites/Hud/Empty.png');
+     this.game.load.spritesheet('coinIcon', 'resources/sprites/Hud/coin.png', 120, 96, 6);
+ 
+     //UNITS
+     this.game.load.image('Red_Peasant', 'resources/sprites/Hud/Red_Peasant.png');
+     this.game.load.image('Red_Lancer', 'resources/sprites/Hud/Red_Lancer.png');
+     this.game.load.image('Red_Swordman', 'resources/sprites/Hud/Red_Swordman.png');
+     this.game.load.image('Red_Horseman', 'resources/sprites/Hud/Red_Horseman.png');
+     this.game.load.image('Yellow_Peasant', 'resources/sprites/Hud/Yellow_Peasant.png');
+     this.game.load.image('Yellow_Lancer', 'resources/sprites/Hud/Yellow_Lancer.png');
+     this.game.load.image('Yellow_Swordman', 'resources/sprites/Hud/Yellow_Swordman.png');
+     this.game.load.image('Yellow_Horseman', 'resources/sprites/Hud/Yellow_Horseman.png');
+ 
+     //STRUCTURES
+     this.game.load.image('Red_Farm', 'resources/sprites/Hud/Red_Farm.png')
+     this.game.load.image('Red_Tower', 'resources/sprites/Hud/Red_Tower.png');
+     this.game.load.image('Red_Fortress', 'resources/sprites/Hud/Red_Fortress.png');
+     this.game.load.image('Red_Base', 'resources/sprites/Hud/Red_Base.png');
+     this.game.load.image('Yellow_Farm', 'resources/sprites/Hud/Yellow_Farm.png');
+     this.game.load.image('Yellow_Tower', 'resources/sprites/Hud/Yellow_Tower.png');
+     this.game.load.image('Yellow_Fortress', 'resources/sprites/Hud/Yellow_Fortress.png');
+     this.game.load.image('Yellow_Base', 'resources/sprites/Hud/Yellow_Base.png');
+ 
+     //INDICATORS
+     this.game.load.image('movement', 'resources/sprites/Movement_Indicator.png');
+     this.game.load.image('nope', 'resources/sprites/Nope_Indicator.png');
+     this.game.load.image('combat', 'resources/sprites/Combat_Indicator.png');
+  },
+  create:function(){
+    this.background = this.game.add.image(0, 0, 'backgroundImage');
+    this.playButton = this.game.add.button(50, 475, 'playButton', this.PlayStart, this, 1, 1, 0);
+    this.ruleButton = this.game.add.button(250, 475, 'rulesButton', this.HowToPlayStart, this, 1, 1, 0);
+    this.aboutUsButton = this.game.add.button(450, 450, 'aboutUsButton', this.AboutUsStart, this, 1, 1, 0);
+  },
+  PlayStart:function(){
+    this.game.state.start('play');
+  },
+  HowToPlayStart:function(){
+    this.game.state.start('How to Play');
+  },
+  AboutUsStart:function(){
+    this.game.state.start('About Us');
+  }
+};
+
+var AboutUsScene={
+  create:function(){
+    this.aboutUsImage = this.game.add.image(0, 0, 'aboutUs');
+    this.backButton = this.game.add.button(100, 500, 'backButton', this.back, this, 2, 1, 0);
+  },
+  back:function(){
+    this.game.state.start('MainMenu');
+  }
+}
+
+var HowToPlayScene={
+  create:function(){
+    this.HowToPlayImage = this.game.add.image(0, 0, 'rules');
+    this.backButton = this.game.add.button(500, 515, 'backButton', this.back, this, 2, 1, 0);
+  },
+  back:function(){
+    this.game.state.start('MainMenu');
+  }
+}
+
+//Falta implementar al terminar el juego
+var EndOfGame={
+  create:function(){
+    this.game.add.image(0, 0, 'endGame');
+    this.menuButton = this.game.add.button(500, 100, 'backButton', this.backMenu, this, 2, 1, 0);
+  },
+  backMenu:function(){
+    this.game.state.start('MainMenu');
+  }
+}
 
 window.onload = function () {
   var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
   game.state.add('boot', BootScene);
   game.state.add('preloader', PreloaderScene);
+  game.state.add('MainMenu', MenuScene);
   game.state.add('play', PlayScene);
+  game.state.add('How to Play', HowToPlayScene);
+  game.state.add('EndGame', EndOfGame);
+  game.state.add('About Us', AboutUsScene)
 
   game.state.start('boot');
 };
